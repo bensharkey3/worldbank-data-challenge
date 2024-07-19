a3d0fee8d1cd8c17c296bc24f969524af918f770
Ben Sharkey <bensharkey3@gmail.com>
2024-07-19 13:58:12 +1000

    completed query

--- dbt/models/query.sql
+++ dbt/models/query.sql
@@ -1,15 +1,53 @@
-with
-temp1 as (
-    select
-        country_value as country,
-        date as year,
-        value as gdp,
-        lag(
-            value
-        ) over(partition by countryiso3code order by date asc) as prev_gdp
-    from public.gdp_alldata
-    where country_value != ''
-    order by country_value, date
+WITH
+stg_gdp_alldata as (
+	select
+        country_value AS country,
+        date AS year,
+        value AS gdp
+	from gdp_alldata
+),
+
+add_prev_gdp AS (
+    SELECT
+        country,
+        year,
+        gdp,
+        LAG(
+            gdp
+        ) OVER(PARTITION BY country ORDER BY year ASC) AS prev_gdp
+    FROM stg_gdp_alldata
+    WHERE country != ''
+	and gdp != 'NaN'
+    ORDER BY country, year
+),
+
+add_gdp_growth as (
+	SELECT
+		*,
+		(gdp::float / prev_gdp::float) -1 as gdp_growth
+	FROM add_prev_gdp
+),
+
+find_min_max_growths as (
+	select
+		country,
+		min(gdp_growth) as min_gdp_growth_since_2000,
+		max(gdp_growth) as max_gdp_growth_since_2000
+	from add_gdp_growth
+	where year::int >= 2000
+	group by
+		country
+),
+
+final as (
+	select
+		a.country,
+		a.year,
+		a.gdp,
+		b.min_gdp_growth_since_2000,
+		b.max_gdp_growth_since_2000
+	from add_gdp_growth as a
+	join find_min_max_growths as b on b.country = a.country
 )
 
-select * from temp1
+select * from final
